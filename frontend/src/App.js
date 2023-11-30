dd17e7074dbde00e266f93e81cbcf8f6772de1a7
Ruilin P's ArchLinux <ruilin.peng@mail.utoronto.ca>
2023-11-30 17:45:50 -0500

    updated App.js to react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PetList from './components/pets';
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>PetPal</h1>
-      </header>
-      <main>
-        <PetList />
-      </main>
-    </div>
-  );
+  return <BrowserRouter>
+  <Routes>
+  <Route path="/pets/" element={<PetList />} />
+    {/* <Route path="/login/" exact element={<Login />} /> */}
+    {/* <Route path="/" element={<Layout />}> */}
+      {/* <Route index element={<Landing />} /> */}
+      
+      {/* <Route path="topic/:topicId/" element={<Topic />} /> */}
+    {/* </Route> */}
+  </Routes>
+</BrowserRouter>;
 }
 
 export default App;

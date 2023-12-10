a2ef44458f36d13413ec3014d7eb83e15427498b
Ruilin Peng <93541585+RuilinP@users.noreply.github.com>
2023-12-10 02:35:09 -0500

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -18,7 +18,7 @@ function Login() {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:8000/api/token/', {
+            const response = await fetch('http://127.0.0.1:8000/api/token/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
